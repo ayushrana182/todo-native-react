bba97113abf2ded0128b49162a0135102e6264e3
ayushrana182 <ayushrana182@gmail.com>
2021-02-12 08:47:19 +0545

    React.useState

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { Button, Input, Flex, Checkbox, Heading } from "@chakra-ui/react";
+import { SetTodosType, TodosType } from "../store";
+
+function TodoListItems( {todos, todosSet}: {todos: TodosType, todosSet: SetTodosType}) {
+  return (
+    <>
+      {[].map((todo: { id: number; text: string }) => (
+        <Flex pt={2} key={todo.id}>
+          <Checkbox />
+          <Input mx={2} value={todo.text} />
+          <Button>Delete</Button>
+        </Flex>
+      ))}
+    </>
+  );
+}
+
+function TodoList({todos, todosSet}: {todos: TodosType, todosSet: SetTodosType}) {
+  return (
+    <>
+      <Heading>Todo List</Heading>
+      <TodoListItems todos={todos} todosSet={todosSet}/>
+    </>
+  );
+}
+
+export default TodoList;
